c36030ceedc807002a0e4d23885174edb6b18311
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 14:42:54 +0000

    Automatically backed up by Learn

--- board.rb
+++ board.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts "   |   |   "
 end
 
-board = " "," "," "," "," "," "," "," "," "," "," "," "
+board = [" "," "," "," "," "," "," "," "," "," "," "," "]
 display_board(board)
