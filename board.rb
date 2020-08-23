a9e6ed190ebfc9623cb0be5fb4e64719b46365bf
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 14:45:48 +0000

    Automatically backed up by Learn

--- board.rb
+++ board.rb
@@ -4,6 +4,7 @@ def display_board(new)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  return display board(new)
 end
 
 board = [" "," "," "," "," "," "," "," "," "," "," "," "]
