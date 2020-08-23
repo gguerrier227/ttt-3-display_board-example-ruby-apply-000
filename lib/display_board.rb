a16f3e4da2661c062d97eaff63d52dc89789dfa7
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 13:50:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,19 @@
 puts    "Welcome to Tic Tac Toe"
 
 def display_board
+<<<<<<< HEAD
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
+=======
+puts "   |  |  "
+puts "-----------"
+puts "   |  |  "
+puts "-----------"
+puts "   |  |  "
+>>>>>>> 18182869e55f0f2eba22e7dab5e612e7b654570d
 end
 
 puts display_board
